8e30643e58ce5032eb3525434ce272c3bac6e4f2
Neeharika Baral <neehabaral72@gmail.com>
2023-11-26 20:29:27 -0500

    implement loadTweets function to fetch tweets from server. remove hard-coded data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-]
 
 $(document).ready( function() {
   const createTweetElement = function(tweet) {
@@ -60,23 +36,29 @@ $(document).ready( function() {
       $('#tweets-container').append(newTweet);
     }
   }
-  
-  renderTweets(data);
 
+  // Use ajax POST request to send serialized data to server (server is configured to receive data in query string)
   $('.new-tweet form').on('submit', function(event) {
     event.preventDefault();
-
-    // serialize the form data (tweeter server is configured to receive data in query string format)
     const formData = $('.new-tweet form').serialize();
-
-    // ajax request: send serialized data to server by submitting post request
     $.ajax({
     type: "POST",
     url: '/tweets/',
     data: formData
     });
-
   })
+
+  // Use ajax GET request to retrieve data from server
+  const loadTweets = function () {
+    $.ajax({
+      type: "GET",
+      url: "/tweets/",
+      success: (tweets) => {
+        renderTweets(tweets);
+      }
+    })
+  };
+  loadTweets();
 
 });
 
